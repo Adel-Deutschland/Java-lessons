09ca44a0286266d126a6e4b8cc675c9834a554eb
Adolat Kodirova <kodirovaadolat@gmail.com>
2025-05-07 14:29:12 +0200

    update

---
+++ GuessAge.java
@@ -0,0 +1,11 @@
+public class GuessAge {
+    public static void main(String[] args) {
+        int age = 19;  // Пример возраста
+
+        if (age >= 18) {
+            System.out.println("Вы совершеннолетний");
+        } else {
+            System.out.println("Вы несовершеннолетний");
+        }
+    }
+}
