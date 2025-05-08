b8384317f81d809e5cbd10b90bd49cd1550da427
Adolat Kodirova <kodirovaadolat@gmail.com>
2025-05-08 11:33:08 +0200

    added java files

---
+++ training/CheckAge.java
@@ -0,0 +1,11 @@
+public class CheckAge {
+  public static void main (String[] args ) {
+    int age = 5;
+
+    if (age >= 18) {
+      System.out.println("Vi vzrosliy");
+    } else {
+      System.out.println("Vam eshe net 18");
+    }
+  }
+}
