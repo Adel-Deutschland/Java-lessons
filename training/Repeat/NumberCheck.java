6002538382910dfb3b4a527005531aba8ae3d598
Adolat Kodirova <kodirovaadolat@gmail.com>
2025-06-22 13:21:15 +0500

    update

---
+++ training/Repeat/NumberCheck.java
@@ -0,0 +1,9 @@
+public class NumberCheck {
+  public static void main (String [] args) {
+    int number = -5;
+    if (number >= 0) {
+      System.out.println ("Chislo polojitelnoye");
+    } else { 
+      System.out.println("Chislo otriyatelnoye");
+    }
+    }}
