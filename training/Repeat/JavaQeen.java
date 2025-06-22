8c133983091a9466e7b7b46f312128387c610cf0
Adolat Kodirova <kodirovaadolat@gmail.com>
2025-06-22 12:47:05 +0500

    update

---
+++ training/Repeat/JavaQeen.java
@@ -0,0 +1,8 @@
+public class JavaQeen {
+  public static void main (String [] args) {
+    String firstName ="Adolia";
+    String lastName ="JavaQeen";
+    String fullName =firstName + " " +lastName;
+    System.out.println("Polnoye imya: " + fullName);
+  }
+}
---
+++ training/Repeat/one.java
@@ -0,0 +1,14 @@
+public class one {
+  public static void main (String [] args) {
+    int age = 20;
+    double price = 19.99;
+    String name = "Adolia";
+    boolean isHappy = true;
+
+    System.out.println(age);
+    System.out.println(price);
+    System.out.println(name);
+    System.out.println(isHappy);
+  }
+}
+
