a2e38f36fdc20e427ba5e0b1cedbd552f6708f92
Adolat Kodirova <kodirovaadolat@gmail.com>
2025-07-09 12:15:23 +0200

    update

---
+++ training/Repeat/println.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class println {
+  public static void main (String [] args){
+    String name ="Adolya";
+    int age = 29;
+    String city = "Berlin";
+
+    System.out.println ("Privet," + name + "! Tebe " +  age + " let i ti jivesh v gorode " +  city + ".");
+  } 
+}
