9a21dbc06b3cee5bc25ddb0858a7a1b537ef72a4
Adolat Kodirova <kodirovaadolat@gmail.com>
2025-07-09 11:26:59 +0200

    update

---
+++ training/Repeat/HelloVariables.java
@@ -0,0 +1,13 @@
+public class HelloVariables {
+  public static void main(String [] args) {
+    int age = 25;
+    double price = 9.99;
+    String name ="Adolat";
+    boolean isHappy = true;
+
+    System.out.println("Imya: " + name);
+    System.out.println("Vozvrast:" + age);
+    System.out.println("Zena:" + price);
+    System.out.println("Schastliva:" + isHappy);
+  }
+}
