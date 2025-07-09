48f5c4482e0c83c9f6a75e3c8ff0c788b713be69
Adolat Kodirova <kodirovaadolat@gmail.com>
2025-07-09 11:34:04 +0200

    update

---
+++ training/Repeat/Temperature.java
@@ -0,0 +1,10 @@
+public class Temperature {
+  public static void main (String [] args) {
+    int temperature =30;
+    if (temperature >= 25){
+      System.out.println("Na ulize jarko!");
+      } else {
+        System.out.println("Pogoda normalnaya");
+      }
+  }
+}
