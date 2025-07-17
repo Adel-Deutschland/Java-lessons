44b4089d7f37beabb04afb90fec278f7f7da6007
Adolat Kodirova <kodirovaadolat@gmail.com>
2025-07-17 14:30:14 +0200

    update

---
+++ training/Massivi/exp1.java
@@ -0,0 +1,12 @@
+
+public class exp1 {
+  public static void main (String [] args) {
+
+ 
+
+  int [] numbers = {5, 10, 15, 20};
+
+  System.out.println (numbers[0]);
+  System.out.println (numbers[2]);
+}
+ }
